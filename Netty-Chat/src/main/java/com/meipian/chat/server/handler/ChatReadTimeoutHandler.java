820af9bcd7e99c72ae57da4379c1c76c5aee1914
741691740 <741691740@qq.com>
2017-04-16 00:08:25 +0800

    超时

---
+++ Netty-Chat/src/main/java/com/meipian/chat/server/handler/ChatReadTimeoutHandler.java
@@ -0,0 +1,35 @@
+package com.meipian.chat.server.handler;
+
+import java.util.concurrent.TimeUnit;
+
+import com.meipian.chat.server.core.SessionManager;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelPromise;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+
+/**
+ * 超时要清空session
+ * 
+ * @author xujianxing
+ *
+ */
+public class ChatReadTimeoutHandler extends ReadTimeoutHandler {
+
+	public ChatReadTimeoutHandler(long timeout, TimeUnit unit) {
+		super(timeout, unit);
+	}
+
+	@Override
+	protected void readTimedOut(ChannelHandlerContext ctx) throws Exception {
+		super.readTimedOut(ctx);
+		SessionManager.getInstance().getSessionByChannelHandlerContext(ctx).destory();
+
+	}
+
+	@Override
+	public void disconnect(ChannelHandlerContext ctx, ChannelPromise promise) throws Exception {
+		super.disconnect(ctx, promise);
+	}
+
+}
